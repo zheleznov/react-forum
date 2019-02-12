71c728186637371dc84c3a426a2987fa05d958ba
Roman Zhelieznov <rzheleznov82@outlook.com>
2019-02-12 23:12:31 +0200

    update store

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -2,7 +2,7 @@ import actions from './actions';
 import store from '../../store';
 
 export default {
-  getCategories() {
-    return store.dispatch(actions.getCategories());
+  getData() {
+    return store.dispatch(actions.getData());
   }
 };
